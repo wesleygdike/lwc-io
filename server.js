b356d74fd3355fcb8076119aa16b8bdf82a5396e
Wesley G Dike <wesley.g.dike@gmail.com>
2021-06-21 09:12:19 -0400

    updated server code

--- server.js
+++ server.js
@@ -13,10 +13,57 @@ const server = express()
 const io = socketIO(server);
 
 io.on('connection', (socket) => {
-  console.log('Client connected Socket Id: ' + socket.id);
+  console.log('Client connected');
+
+  /**
+   * Create function to send status
+   * @param success {bool}
+   * @param message {string}
+   */
+  const sendStatus = function({success, message}){
+    socket.emit('status', {success, message});
+  }
+
+  socket.on('transmit', () => {
+    io.emit('chatupdated');
+  });
+
+  socket.on('usertyping', (data) => {
+    io.emit('istyping', data);
+  });
+
+  socket.on('userEnteredChat', () => {
+    io.emit('refreshChatUsers');
+  });
+
+  socket.on('userLeftChat', () => {
+    io.emit('refreshChatUsers');
+  });
+
+  socket.on('usernottyping', (data) => {
+    io.emit('nottyping', data);
+  });
+
+  socket.on('input', (data) => {
+    const name = data.name;
+    const message = data.message;
+
+    if (!name || !message) {
+      sendStatus({
+        success: false,
+        message: 'Please enter a name and message'
+      });
+    } else {
+      socket.emit('output', data);
+      sendStatus({
+        success: true,
+        message: 'Message updating...'
+      });
+    }
+  })
 
   socket.on('disconnect', () => console.log('Client disconnected'));
-  socket.on('clicked', () => console.log('Button clicked by client'));
+
 });
 
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
